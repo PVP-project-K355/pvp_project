7729853c3cd52f72cfe09f46645b55f92d7f6640
karmoc15 <126975238+karmoc15@users.noreply.github.com>
2024-05-02 12:45:43 +0300

    bugs fix

--- app/src/main/java/app/app/MainActivity.kt
+++ app/src/main/java/app/app/MainActivity.kt
@@ -116,7 +116,7 @@ class MainActivity : AppCompatActivity() {
                 birthdate = "1900-01-01" // Assuming birthdate is in "YYYY-MM-DD" format
             )
             //Inserting user data into the database
-            dbHelper.addUser(newUser)
+            //dbHelper.addUser(newUser)
         }
         else{
             println("User exists")
@@ -129,7 +129,7 @@ class MainActivity : AppCompatActivity() {
                 maxRate = 100
             )
             //Inserting threshold data into the database
-            dbHelper.addThreshold(newThreshold)
+            //dbHelper.addThreshold(newThreshold)
         }
         else{
             println("Threshold exists")
@@ -145,7 +145,7 @@ class MainActivity : AppCompatActivity() {
                 expiresIn = 365
             )
             //Inserting api data into database
-            dbHelper.addApi(newApi)
+            //dbHelper.addApi(newApi)
         }
         else{
             println("API exists")
@@ -200,7 +200,7 @@ class MainActivity : AppCompatActivity() {
         //Checking if inserted heart rate is between thresholds
         //CheckData(this).checkRate(insertedRateId.toInt(), 1)
 
-        val apiID = 1 // Replace with the ID of the user you want to retrieve
+        /*val apiID = 1 // Replace with the ID of the user you want to retrieve
         val api = dbHelper.getApi(apiID)
 
         if (api != null) {
@@ -213,7 +213,7 @@ class MainActivity : AppCompatActivity() {
             println("Expires in: ${api.expiresIn}")
         } else {
             println("API not found.")
-        }
+        }*/
 
     }
 
